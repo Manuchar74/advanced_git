716ca0302b8bded0dd23ac185a9449ad72c3dbe8
Manuchar <manucharmuseridze@gmail.com>
2024-07-01 02:02:46 +0300

    init_4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
-from src.widget import get_data
+from src.widget import mask_account_card, get_data
+
 
 def main() -> None:
     items = mask_account_card('VisaPlatinum7000792289606361')
